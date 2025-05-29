b83d8f0bc95239e3709b1d814702f9d02d084340
Taha <166891858+Tahaawh@users.noreply.github.com>
2025-05-29 13:29:14 +0330

    Update test today.py

--- test today.py
+++ test today.py
@@ -1,20 +1,28 @@
+import requests
+from requests.models import Response
 from bs4 import BeautifulSoup
-from requests import Response
 from arabic_reshaper import reshape
 from bidi.algorithm import get_display
 
-def extract_site(response: Response):
-    content = BeautifulSoup(response.content, "html.parser")  # اصلاح "html-parser" به "html.parser"
-    div_main = content.find("div", attrs={"class": "view-content"})
-    all_tables = div_main.find_all("table")  # find_all خودش لیست میده؛ نیازی به *list نداری
+def check_website() -> Response | str:
+    response = requests.get("http://chap-sch.ir/guide-books")
+    if response.status_code in [200, 201, 202]:
+        return response
+    return "not connect"
 
+def extract_site(response: Response):
+    content = BeautifulSoup(response.content, "html.parser")
+    div_main = content.find("div", attrs={"class": "view-content"})
+    all_tables = div_main.find_all("table")
     for every_table in all_tables:
         all_tr = every_table.find_all("tr")
         for every_tr in all_tr:
             all_tds = every_tr.find_all("td")
             for every_td in all_tds:
-                print(get_display(reshape(every_td.text)))
+                print(get_display(reshape(every_td.text.strip())))
 
-# فرض می‌کنم check_website() تابعی هست که response میده
 response = check_website()
-extract_site(response)
+if isinstance(response, Response):
+    extract_site(response)
+else:
+    print(response)
